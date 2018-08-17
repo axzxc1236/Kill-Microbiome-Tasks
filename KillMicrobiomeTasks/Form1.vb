3c78f676c3024f6e030fb387d7f2463e36feb2e8
axzxc1236 <axzxc1236@gmail.com>
2018-08-17 16:38:44 +0800

    Fix broken "check GUI RPC key" function.

--- KillMicrobiomeTasks/Form1.vb
+++ KillMicrobiomeTasks/Form1.vb
@@ -7,17 +7,15 @@
         End If
         Dim rpcClient As New BoincRpc.RpcClient,
             key = My.Computer.FileSystem.ReadAllText("RPCkey.txt")
-        Try
-            Await rpcClient.AuthorizeAsync(key)
-        Catch ex As System.InvalidOperationException
-            MsgBox("I don't have permission to stop BOINC tasks, please make sure RPCkey.txt has the right token/password to interact with BOINC.")
-            End
-        End Try
         If Not My.Computer.FileSystem.FileExists("boinccmd.exe") Then
             MsgBox("This program has to be place in the same folder where boinccmd.exe exists to run.", MsgBoxStyle.Critical)
             End
         End If
         Await rpcClient.ConnectAsync("localhost", 31416)
+        If Not Await rpcClient.AuthorizeAsync(key) Then
+            MsgBox("I don't have permission to stop BOINC tasks, please make sure RPCkey.txt has the right token/password to interact with BOINC.")
+            End
+        End If
         While True
             For Each result In Await rpcClient.GetResultsAsync
                 If Not result.State = 6 And result.Name.StartsWith("MIP1") Then  'result.State = 6  =>  aborted
